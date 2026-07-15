2bbb2a60b56f571e75b30017db010b4e36f35ca1
whisper011 <solomonjigah@yahoo.com>
2026-07-15 05:49:20 +0000

    feat: define ContractError enum

--- src/errors.rs
+++ src/errors.rs
@@ -1,1 +1,12 @@
-// ContractError enum.
+use soroban_sdk::contracterror;
+
+/// Errors returned by the contract registry.
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum ContractError {
+    AlreadyRegistered = 1,
+    NotFound = 2,
+    Unauthorized = 3,
+    InvalidInput = 4,
+}
